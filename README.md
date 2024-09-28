b17c72ca2b5de34c9a152986410f2e9b735840c4
FelipeHoffmannV <hoffmannfelipe940@gmail.com>
2024-09-28 10:42:38 -0300

    Primeira versão do sistema.
    Conclusões da versão inseridas no arquivo Readme.md.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Projeto_integrado_inovacao_1
 Projeto realizado no âmbito acadêmico, com intuito de praticar conceitos interdicsiplinares do semestre.
+
+        ----- Dia 28/09/2024 -----
+
+- Realizada a criação do banco de dados que contem uma tabela listando os produtos presentes em estoque.
+- Realizado um teste simples para grantir que os produtos estão sendo adicionados ao  Data Base com sucesso.
+
+-- Conclusão do teste: O programa consegue inserir os dados de forma eficiente, mas produtos duplicados são inseridos também.
---
+++ main.py
@@ -0,0 +1,67 @@
+# Biblioteca sqlite3
+import sqlite3
+
+# Estrutura de dados com conexão ao banco de dados.
+
+class ItemEstoque:
+    def __init__(self, nome, categoria, quantidade, preco, localizacao):
+        self.nome = nome
+        self.categoria = categoria
+        self.quantidade = quantidade
+        self.preco = preco
+        self.localizacao = localizacao
+
+# Conexão ao banco de dados
+class GerenciamentoEstoque:
+    def __init__(self, db_name = 'estoque.db'):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.criar_tabela()
+
+# Criação de uma tabela no bando de dados
+    def criar_tabela(self):
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS produtos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            categoria TEXT NOT NULL,
+            quantidade INTEGER NOT NULL,
+            preco REAL NOT NULL,
+            localizacao TEXT NOT NULL
+        )
+        ''')
+        self.conn.commit()
+
+
+# funcão para adicionar produtos ao banco de dados
+    def adicionar_produto(self, produtos):
+        self.cursor.execute('''
+        INSERT INTO produtos (nome, categoria, quantidade, preco, localizacao)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (produtos.nome, produtos.categoria, produtos.quantidade, produtos.preco, produtos.localizacao))
+        self.conn.commit()
+
+# funcão para consultar produtos no banco de dados
+    def consultar_produtos(self):
+        self.cursor.execute("SELECT * FROM produtos")
+        return self.cursor.fetchall()
+
+# Fechanco conexão com o banco de dados.
+    def Fechar_conexao(self):
+        self.conn.close()
+
+
+
+
+
+# testes
+
+controle = GerenciamentoEstoque()
+
+Produto1 = ItemEstoque('Motorola Moto G-10' , 'Celulares', 50 , 250.25 , 'setor C, fila M, posicao 15')
+
+controle.adicionar_produto(Produto1)
+
+produtos = controle.consultar_produtos()
+for produto in produtos:
+    print(f'ID: {produto[0]}, nome: {produto[1]}, categoria: {produto[2]}, quantidade:{produto[3]}, preço: {produto[4]}, Localização: {produto[5]}')
