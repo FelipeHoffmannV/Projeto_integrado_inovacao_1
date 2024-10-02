e7fb320c1f5ea0171ef2db176a5cf3736e28150e
FelipeHoffmannV <hoffmannfelipe940@gmail.com>
2024-10-02 18:37:19 -0300

    Atualização 0.3
    O sistema está mais robusto (para o meu nível de conhecimento). Mais informações no arquivo README.md.

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ Projeto realizado no âmbito acadêmico, com intuito de praticar conceitos inter
 - Teste para verificar se o produto é removido pelo seu id no banco de dados relaizado.
 
 -- Conclusão: a função remove o produto do banco de dados utilizando a entrada do id do produto. Produtos duplicados ainda estão sendo inseridos, até o fim do projeto deve ser resolvido, por agora em fase de desenvolvimento e teste não é um grande problema.
+
+----- Dia 02/10/2024 -----
+
+- O sistema recebe dados digitados pelo usuário e consegue realizar funções de adicionar produtos, remover produtos, alterar quantidade e alterar preço com êxito. Também é possível vizualizar os produtos em estoque com todas suas informações.
+
+- O programa atende quase todos os requisitos dispostos na atividade, porém falta implementar a elaboração de relatórios aonde mostram os itens com estoque baixo e itens com estoque muito elevado. 
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-# Biblioteca sqlite3
+# Importando banco de dados Sqlite3
 import sqlite3
 
-# Estrutura de dados com conexão ao banco de dados.
-
+# Classe que cria o objeto produto
 class ItemEstoque:
-    def __init__(self, nome, categoria, quantidade, preco, localizacao):
+    def __init__(self, nome, categoria, quantidade, preco, posicao):
         self.nome = nome
         self.categoria = categoria
         self.quantidade = quantidade
         self.preco = preco
-        self.localizacao = localizacao
+        self.posicao = posicao
 
-# Conexão ao banco de dados
+
+# Classe para gerenciamento do estoque
+
 class GerenciamentoEstoque:
     def __init__(self, db_name = 'estoque.db'):
         self.conn = sqlite3.connect(db_name)
@@ -27,7 +28,7 @@ class GerenciamentoEstoque:
             categoria TEXT NOT NULL,
             quantidade INTEGER NOT NULL,
             preco REAL NOT NULL,
-            localizacao TEXT NOT NULL
+            posicao TEXT NOT NULL
         )
         ''')
         self.conn.commit()
@@ -36,16 +37,23 @@ class GerenciamentoEstoque:
 # funcão para adicionar produtos ao banco de dados
     def adicionar_produto(self, produtos):
         self.cursor.execute('''
-        INSERT INTO produtos (nome, categoria, quantidade, preco, localizacao)
+        INSERT INTO produtos (nome, categoria, quantidade, preco, posicao)
         VALUES (?, ?, ?, ?, ?)
-        ''', (produtos.nome, produtos.categoria, produtos.quantidade, produtos.preco, produtos.localizacao))
+        ''', (produtos.nome, produtos.categoria, produtos.quantidade, produtos.preco, produtos.posicao))
         self.conn.commit()
 
-# função para remover produtos da tabela do banco de dados
     # função para remover produtos da tabela do banco de dados
     def remover_produto(self, produto_id):
         self.cursor.execute('''DELETE FROM produtos WHERE id = ?''', (produto_id,))
         self.conn.commit()
+
+    # funçãopara alterar quantidade de produtos
+    def alterar_quantidade(self,produto_id, nova_quantidade):
+            self.cursor.execute(f''' UPDATE produtos SET quantidade = {nova_quantidade} WHERE id = {produto_id} ''')
+    
+    # função para alterar preço dos produtos
+    def alterar_preco(self, produto_id, novo_preco):
+        self.cursor.execute(f''' UPDATE produtos SET preco = {novo_preco} WHERE id = {produto_id} ''')
 
 # funcão para consultar produtos no banco de dados
     def consultar_produtos(self):
@@ -57,23 +65,58 @@ class GerenciamentoEstoque:
         self.conn.close()
 
 
+# Inicio da lógica para selecionar o que fazer no sistema.
 
-
-
-# testes
-
+# Variável que recebe a chave para interagir com o estoque
 controle = GerenciamentoEstoque()
 
-Produto1 = ItemEstoque('Motorola Moto G-10' , 'Celulares', 50 , 250.25 , 'setor C, fila M, posicao 15')
-Produto2 = ItemEstoque('Notebook Asus Zenbook' , 'Computadores', 73 , 1500 , 'setor N, fila Z, posicao 26')
+while True:
+    print('Sistema de gerenciamento de estoque')
+    print('---------------------------------------------------')
+    print('Para cadastrar um novo produto digite o número 1')
+    print('---------------------------------------------------')
+    print('Para remover um produto digite o número 2')
+    print('---------------------------------------------------')
+    print('Para alterar a quantidade de um produto digite o número 3')
+    print('---------------------------------------------------')
+    print('Para alterar o preço de um produto digite o número 4')
+    print('---------------------------------------------------')
+    print('Para vizualizar seu estoque digite o número 5')
+    print('---------------------------------------------------')
+    print('Ao terminar as atividades no sistema digite 6 para encerrar')
+    print('---------------------------------------------------')
 
-controle.adicionar_produto(Produto1)
-controle.adicionar_produto(Produto2)
+    atividade = int(input('Digite o número correspondente ao que deseja fazer: '))
 
-# Teste para remover produto
-controle.remover_produto(1)
+    if atividade == 6:
+        print('Encerrando....')
+        break
+    elif atividade == 1:
+        produto = ItemEstoque(input('Digite o nome do produto: '), input('Digite a categoria do produto (exemplo: Celular, Computador, Carregador, etc): '), int(input('Digite a quantidade que será adicionado ao estoque (POR FAVOR UTILIZE APENAS NÚMEROS INTEIROS.): ')), float(input('Digite o valor do produto: ')), input('Digite a posição aonde este produto será alocado no estoque (exemplo: Setor A, Fila C, Posição C): '))
+        controle.adicionar_produto(produto)
+        print('Produto adicionado ao seu estoque com sucesso!!!')
+        continue
 
+    elif atividade == 2:
+        controle.remover_produto(int(input('Digite o identificador único do produto a ser removido: ')))
+        print('Produto removido do seu estoque com sucesso!!')
+        continue
 
-produtos = controle.consultar_produtos()
-for produto in produtos:
-    print(f'ID: {produto[0]}, nome: {produto[1]}, categoria: {produto[2]}, quantidade:{produto[3]}, preço: {produto[4]}, Localização: {produto[5]}')
+    elif atividade == 3:
+        controle.alterar_quantidade(int(input('Digite o identificador único do produto que deseja alterar a quantidade em estoque: ')), int(input('Digite o valor da nova quantidade: ')))
+        print('Qantidade alterada com sucesso!!')
+        continue
+    
+    elif atividade == 4:
+        controle.alterar_preco(int(input('Digite o identificador único do produto que deseja alterar o preço: ')), float(input('Digite o novo preço do produto: ')))
+        print('Preço do produto alterado com sucesso!!')
+
+    elif atividade == 5:
+        produtos = controle.consultar_produtos()
+        for produto in produtos:
+            print(f'ID: {produto[0]}, nome: {produto[1]}, categoria: {produto[2]}, quantidade:{produto[3]}, preço: {produto[4]}, Localização: {produto[5]}')
+
+    else:
+        print('Número digitado não corresponde a uma ação no sistema. Por favor tente um dos números listados abaixo!')
+        print('______________________________________________________________________________________________________________________')
+
